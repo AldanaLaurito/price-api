3e5fbc3bb98d153c2bb3c631f0beb9a90153a690
Aldana Laurito <aldana.laurito.AL@gmail.com>
2024-01-17 14:46:56 -0300

    Added OpenApi annotation to App class

--- src/main/java/com/aldanalaurito/priceapi/App.java
+++ src/main/java/com/aldanalaurito/priceapi/App.java
@@ -1,10 +1,13 @@
 package com.aldanalaurito.priceapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Prices", version = "1.0.0", description = "Prices Api"))
 public class App {
 
     public static void main(String[] args) {
