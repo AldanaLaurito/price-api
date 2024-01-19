1ab5d0ad02877b3110d4155cf09ba2fec0b5fb54
Aldana Laurito <aldana.laurito.AL@gmail.com>
2024-01-19 13:09:55 -0300

    Applied Sonar suggestion

--- src/main/java/com/aldanalaurito/priceapi/helper/Constants.java
+++ src/main/java/com/aldanalaurito/priceapi/helper/Constants.java
@@ -1,6 +1,7 @@
 package com.aldanalaurito.priceapi.helper;
 
 public class Constants {
+    private Constants(){}
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 
     public static final String PRODUCT_PRICE_RESPONSE_DTO_EXAMPLE = "{\"PRODUCT_ID\":35455,\"BRAND_ID\":1,\"PRICE_LIST\":1,\"PRICE\":35.5, \"CURR\":\"EUR\",\"START_DATE\":\"2020-06-14T00:00:00\",\"END_DATE\":\"2020-12-31T23:59:59\"}";
